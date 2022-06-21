b2da57e8928a663bac4d00c87024b03783adf6eb
tooth Less <toothless042@gmail.com>
2022-06-21 14:40:33 +0900

    my first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://toothless042:rlaeorjs7741@boilerplate.1hzp7.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
+.then(() => console.log("MongoDB Connected!"))
+.catch(err => console.log(err));
+
+app.get('/', (req,res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`Example App Listening on Port  ${port}!`));
+
